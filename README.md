39dfd3d159ef0450c0e22c67c9ce7e8f0066615c
Joseph Worman <jdworman84@gmail.com>
2018-07-25 10:09:15 -0400

    created weather temperature

---
+++ README.md
@@ -0,0 +1,1 @@
+By using variables, my program should work for any Kelvin temperature — just change the value of kelvin and run the program again.
---
+++ main.js
@@ -0,0 +1,5 @@
+const kelvin = 293; //kelvin is equal to 293//
+const celsius = kelvin - 273; //Kelvin is similiar to Celsius//
+let fahrenheit = celsius * (9 / 5) + 32; //calculate farenheit//
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
